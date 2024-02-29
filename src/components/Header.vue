8126b3b9ce5b75b634f1dddde62125358381b358
Jennifer Echenim <echenimjennifer@gmail.com>
2024-02-29 18:05:24 +0400

    wip: upgrade ui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,32 @@
-<script setup lang="ts">
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+  >
+    <el-menu-item index="0">
+      <span> Welcome to Spot the Ball </span>
+    </el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-sub-menu index="2">
+      <template #title>Workspace</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-sub-menu index="2-4">
+        <template #title>item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
 import { ref, watchEffect } from 'vue'
 import { formatEther } from 'ethers/lib/utils'
 import MetaMaskConnectButton from './MetaMaskConnectButton.vue'
@@ -8,6 +36,10 @@ const gameStore = useGameStore()
 const prizePool = ref('')
 const timeLeft = ref()
 const gameActive = ref(false)
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 
 const showPreviousMoves = (event: Event) => {
   gameStore.showPreviousMoves = (event.target as HTMLInputElement).checked
@@ -20,7 +52,7 @@ watchEffect(() => {
 })
 </script>
 
-<template>
+<!-- <template>
   <div class="wrapper">
     <div class="py-8 flex items-start justify-between">
       <div class="flex justify-between gap-4">
@@ -53,4 +85,4 @@ watchEffect(() => {
       </div>
     </div>
   </div>
-</template>
+</template> -->

9bee3e2e68d614b1aace3cfde6b3ff8ac92e2850
Jennifer Echenim <echenimjennifer@gmail.com>
2024-03-20 01:31:02 +0400

    rm auth page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Auth from '../views/Auth.vue'
 import Admin from '../views/Admin.vue'
 
-// const base = '/sample-applications/spot-the-ball/'
-const base = ''
-
 const router = createRouter({
-  history: createWebHistory(base),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/auth',
-      name: 'auth',
-      component: Auth
     },
     {
       path: '/admin',
